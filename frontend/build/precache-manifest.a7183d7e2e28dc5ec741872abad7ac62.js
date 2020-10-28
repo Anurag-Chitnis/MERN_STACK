ef2efeb5269f691a7b58584c4edbe226e6ce93c9
Anurag Chitnis <43677925+Anurag-Chitnis@users.noreply.github.com>
2020-10-28 21:17:17 +0530

    Uploading Files

---
+++ frontend/build/precache-manifest.a7183d7e2e28dc5ec741872abad7ac62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153d622231a9b2310b397daaebbb5427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6913c6826918870bc77",
+    "url": "/static/css/main.a370e806.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6913c6826918870bc77",
+    "url": "/static/js/main.6e5133ce.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
